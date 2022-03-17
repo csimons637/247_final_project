a6438f41baef60283288cf1893e963f3977196ee
DiegoG <diegoag@email.sc.edu>
2022-03-17 13:43:12 -0400

    Added the stubs for the room and hotel

---
+++ Room.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Room {
+    public int roomNum;
+    public String roomType;
+    public ArrayList<Date> availabilities;
+    public ArrayList<Date> daysBooked;
+
+    public Room(int roomNum, String roomType){
+        this.roomNum = roomNum;
+        this.roomType = roomType;
+        availabilities = new ArrayList<Date>();
+        daysBooked = new ArrayList<Date>();
+    }
+}
