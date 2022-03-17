5c4e7cc7d1d7c7eb714d4f25e2129358a01afe91
DiegoG <diegoag@email.sc.edu>
2022-03-17 13:14:26 -0400

    Facade created with empty methods

---
+++ FlightBookingFacade.java
@@ -0,0 +1,33 @@
+public class FlightBookingFacade {
+    private Flight flights;
+    private Hotel hotels;
+    
+    public FlightBookingFacade(Flight flights, Hotel hotels){
+        this.flights = flights;
+        this.hotels = hotels;
+    }
+
+    private void bookFlight(){
+
+    }
+
+    private void bookHotel(){
+
+    }
+
+    private void checkReservation(){
+
+    }
+
+    private void changeReservation(){
+
+    }
+
+    private void login(){
+
+    }
+
+    private void createAccount(){
+        
+    }
+}
