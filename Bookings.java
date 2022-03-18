d2bb8bc0d20a0337564e1328c4da1c7b2db77deb
csimons637 <csimons637@gmail.com>
2022-03-18 16:36:28 -0400

    Stubbed Booking and Bookings

--- Bookings.java
+++ Bookings.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+
 public class Bookings implements Booking {
-    
+    private ArrayList<Booking> bookings;
+
+    public ArrayList<Booking> getAllBookings(){
+
+    }
+
+    public Booking getBooking(String keyword) {
+        
+    }
+
 }
