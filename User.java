aee87b7cfee191a8e1c577755e6796d970fa034e
Charles Simons <83017183+csimons637@users.noreply.github.com>
2022-03-20 11:43:04 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.UUID;
 
 abstract class User {
     private String email;
@@ -6,13 +7,15 @@ abstract class User {
     private String lastName;
     private String address;
     private Date birthdate;
+    private ArrayList<UUID> friends;
 
-    public User(String firstName, String lastName, String address, Date birthdate, String email) {
+    public User(String firstName, String lastName, String address, Date birthdate, String email, ArrayList<UUID> friends) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
         this.email = email;
         this.birthdate = birthdate;
+        this.friends = friends;
     }
 
     protected void setFirstName() {
