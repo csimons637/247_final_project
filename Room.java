625b4e8fb739b5e022a156b0c80d57d2acbd9203
Charles Simons <83017183+csimons637@users.noreply.github.com>
2022-03-20 09:44:18 -0400

    Added availabilities ArrayList to constructor

--- Room.java
+++ Room.java
@@ -6,7 +6,7 @@ public class Room {
     public ArrayList<Date> availabilities;
     public ArrayList<Date> daysBooked;
 
-    public Room(int roomNum, String roomType){
+    public Room(int roomNum, String roomType, ArrayList<Date> avail){
         this.roomNum = roomNum;
         this.roomType = roomType;
         availabilities = new ArrayList<Date>();
