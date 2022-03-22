7e39f2893e8d5c279b1547eda1824d0e04511831
DiegoG <diegoag@email.sc.edu>
2022-03-22 13:37:55 -0400

    Returning null for testing

--- Hotels.java
+++ Hotels.java
@@ -1,12 +1,13 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class Hotels {
     private Hotel hotel;
     private ArrayList<Hotels> hotels = new ArrayList<Hotels>();
 
     public ArrayList<Hotels> getAllHotels(){
-        
+        return null;
     }
 
     public ArrayList<Hotels> getHotels(String keywords){
