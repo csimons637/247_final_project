8e14f122f875e29c0013dad441b1fd3ba9ab8772
csimons637 <csimons637@gmail.com>
2022-03-24 16:14:44 -0400

    Added getInstance() to Passports.java

--- Passports.java
+++ Passports.java
@@ -2,8 +2,17 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class Passports {
-    private Passport passport;
+    private static Passports passport;
     private ArrayList<Passport> passports = new ArrayList<Passport>();
+
+    private Passports() {
+        passports = DataLoader.getAllFriends();
+    }
+    public static Passports getInstance() {
+        if (passport == null)
+            passport = new Passports();
+        return passport;
+    }
 
     public ArrayList<Passport> getAllPassports(){
         return null;
