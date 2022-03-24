6354474fa41801ca1d11490a5a1b28d6b23f16df
SheriffHaigler <coodhaigler@gmail.com>
2022-03-24 14:07:59 -0400

    Registered user stuff

--- Hotel.java
+++ Hotel.java
@@ -16,12 +16,7 @@ public class Hotel {
         return this.uuid;
     }
 
-<<<<<<< HEAD
     public String toString(){
         return null;
-=======
-    public String getName() {
-        return this.name;
->>>>>>> a7ebbb63fa4f331a3e0b9c45b6f331c7534f257a
     }
 }
