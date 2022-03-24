c25ba8755c8ca38cf72be128ba3eb34b5e391cd6
DiegoG <diegoag@email.sc.edu>
2022-03-24 13:34:38 -0400

    Changes made to hotels

--- Hotels.java
+++ Hotels.java
@@ -1,16 +1,32 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.UUID;
 
 public class Hotels {
-    private Hotel hotel;
-    private ArrayList<Hotels> hotels = new ArrayList<Hotels>();
+    private static Hotels hotel;
+    private ArrayList<Hotel> hotels;
 
-    public ArrayList<Hotels> getAllHotels(){
-        return null;
+    private Hotels() {
+        hotels = DataLoader.getHotels();
+    }
+
+    public static Hotels getInstance() {
+        if(hotel == null){
+            hotel = new Hotels();
+        }
+        return hotel;
+    }
+
+    public boolean haveHotel(String keyword){
+        return true;
+    }
+
+    public ArrayList<Hotel> getAllHotels(){
+        return DataLoader.getHotels();
     }
 
     public Hotel getHotelByUUID(UUID hotel_id){
-        return null;
+        for(Hotel hotel: hotels) {
+            
+        }
     }
 }
